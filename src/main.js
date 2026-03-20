ebde485c9b3cba004b0c298944ee3c73b738edf4
Julian Tiaden <e241824@edu.sbl.ch>
2026-03-20 14:59:32 +0100

    Import neue Scenen

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import kaplay from "kaplay";
 import sc00 from "./scenes/sc-00";
 import sc01 from "./scenes/sc-01";
+import sc02 from "./scenes/sc-02";
+import gameover from "./scenes/sc-Gameover"
+import win from "./scenes/sc-win"
+import newLevel from "./scenes/sc-newLevel"
 
 const k = kaplay({
 	height: 480,
@@ -14,6 +18,10 @@ const k = kaplay({
 
 k.scene("init", sc00);
 k.scene("lvl-01", sc01);
+k.scene("Gameover", gameover)
+k.scene("lvl-02", sc02)
+k.scene("Win", win)
+k.scene("newLevel", newLevel)
 
 k.go("init");
 
